c617e8322ac3a3ac32e858fcf770df7033be1d73
Junaid Syed <sjunaid626@gmail.com>
2024-02-15 13:48:48 +0530

    show loading message only for meals

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,10 +1,17 @@
+import { Suspense } from 'react';
+
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '../../../lib/meals';
 
-const MealsPage = async () => {
+async function Meals() {
   const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+const MealsPage = () => {
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +27,11 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
