1fc1416195b0c0db75b5ff5328e57295234ba713
Junaid Syed <sjunaid626@gmail.com>
2024-02-17 14:26:19 +0530

    Handle errors while meals fetching

---
+++ src/app/meals/error.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Error() {
+  return (
+    <main className="not-found">
+      <h1>An error occured!</h1>
+      <p>Failed to fetch meals data. Please try again later... </p>
+    </main>
+  );
+}
