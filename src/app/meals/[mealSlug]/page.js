c747f366d5b28c6824d8fd5f51b3fba6995edbe3
Junaid Syed <sjunaid626@gmail.com>
2024-02-23 21:43:28 +0530

    Create the page to show meal details

--- src/app/meals/[mealSlug]/page.js
+++ src/app/meals/[mealSlug]/page.js
@@ -1,8 +1,31 @@
-const MealsDetailsPage = () => {
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '../../../../lib/meals';
+
+const MealsDetailsPage = ({ params }) => {
+  const meal = getMeal(params.mealSlug);
+
   return (
-    <div>
-      <h1>Meal Details</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${'EMAIL'}`}>CREATOR</a>
+          </p>
+          <p className={classes.summary}>SUMMARY</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: '...' }}
+        ></p>
+      </main>
+    </>
   );
 };
 
