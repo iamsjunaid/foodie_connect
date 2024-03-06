9bce00c1a0eeeddbf11ac4ce059bb8ce56be03fb
Junaid Syed <sjunaid626@gmail.com>
2024-03-06 12:57:54 +0530

    Replace image picker component input with a button

--- src/components/meals/image-picker.js
+++ src/components/meals/image-picker.js
@@ -1,6 +1,15 @@
+'use client';
+
+import { useRef } from 'react';
 import classes from './image-picker.module.css';
 
 export default function ImagePicker({ label, name }) {
+  const imageInput = useRef();
+
+  function handlePickClick() {
+    imageInput.current.click();
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
@@ -10,7 +19,16 @@ export default function ImagePicker({ label, name }) {
           id={name}
           accept="image/png, image/jpeg"
           name={name}
+          className={classes.input}
+          ref={imageInput}
         />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an image
+        </button>
       </div>
     </div>
   );
