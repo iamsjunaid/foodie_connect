db4b57cbb36af97d2d9c69d3d7ac2f97662b9c2e
Junaid Syed <sjunaid626@gmail.com>
2024-03-09 13:22:12 +0530

    Add the required prop to the (hidden) <input> element

--- src/components/meals/image-picker.js
+++ src/components/meals/image-picker.js
@@ -47,6 +47,7 @@ export default function ImagePicker({ label, name }) {
           className={classes.input}
           ref={imageInput}
           onChange={handleImageChange}
+          required
         />
 
         <button
